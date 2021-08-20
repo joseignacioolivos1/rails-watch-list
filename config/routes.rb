7a62048a5827e30dee77107b9a4629c39499ccc6
Jose Olivos <jolivos@miucsh.cl>
2021-08-20 03:39:16 -0400

    ejercise movie

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resourses  :movies only [:new,create]
-  resuerces  :lists
-  resourses  :bookmarcks
+  root to: 'artcles#index'
+  resources :articles, except: :index
 end
-
